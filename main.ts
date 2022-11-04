3a4d55cff8d0eaa39570380d94e82253562b944b
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-03 21:31:34 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,3 +3,13 @@ input.onButtonPressed(Button.A, function () {
         basic.showNumber(index)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index <= 9; index++) {
+        basic.showNumber(2 * (index + 1))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index <= 10; index++) {
+        basic.showNumber(2 * index)
+    }
+})
